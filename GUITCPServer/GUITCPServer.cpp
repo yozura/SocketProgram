9578884e14842bc467becd8865be9d84d908010c
yozura <yozura43@Naver.com>
2022-09-10 03:37:52 +0900

    WinAPI

---
+++ GUITCPServer/GUITCPServer.cpp
@@ -0,0 +1,215 @@
+﻿#include "Common.h"
+
+#define SERVERPORT	9000
+#define BUFSIZE		512
+
+// 윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// 에디트 컨트롤 출력 함수
+void DisplayText(const char* fmt, ...);
+
+// 소켓 함수 오류 출력
+void DisplayError(const char* msg);
+
+// 소켓 통신 스레드 함수
+DWORD WINAPI ServerMain(LPVOID arg);
+DWORD WINAPI ProcessClient(LPVOID arg);
+
+HINSTANCE			hInst;
+HWND				hEdit;
+CRITICAL_SECTION	cs;
+
+// 진입점 함수
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	hInst = hInstance;
+	InitializeCriticalSection(&cs);
+
+	// 윈도우 클래스 등록
+	WNDCLASS wndclass;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = _T("MyWndClass");
+	if (!RegisterClass(&wndclass)) return 1;
+
+	// 윈도우 생성
+	HWND hWnd = CreateWindow(_T("MyWndClass"), _T("TCP Server"),
+		WS_OVERLAPPEDWINDOW, 0, 0, 500, 220,
+		NULL, NULL, hInstance, NULL);
+	if (NULL == hWnd) return 1;
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	// 소켓 통신 스레드 생성
+	CreateThread(NULL, 0, ServerMain, NULL, 0, NULL);
+
+	// 메시지 루프
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	DeleteCriticalSection(&cs);
+	return (int)msg.wParam;
+}
+
+// 윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		hEdit = CreateWindow(_T("edit"), NULL,
+			WS_CHILD | WS_VISIBLE | WS_HSCROLL |
+			WS_VSCROLL | ES_AUTOHSCROLL | ES_AUTOVSCROLL |
+			ES_MULTILINE | ES_READONLY,
+			0, 0, 0, 0, hWnd, (HMENU)100, hInst, NULL);
+		return 0;
+	case WM_SIZE:
+		MoveWindow(hEdit, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+// 에디트 컨트롤 출력 함수
+void DisplayText(const char* fmt, ...)
+{
+	va_list arg;
+	va_start(arg, fmt);
+	char cbuf[BUFSIZE * 2];
+	vsprintf(cbuf, fmt, arg);
+	va_end(arg);
+
+	EnterCriticalSection(&cs);
+	int nLength = GetWindowTextLength(hEdit);
+	SendMessage(hEdit, EM_SETSEL, nLength, nLength);
+	SendMessageA(hEdit, EM_REPLACESEL, FALSE, (LPARAM)cbuf);
+	LeaveCriticalSection(&cs);
+}
+
+void DisplayError(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	DisplayText("[%s] %s\r\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// TCP 서버 메인
+DWORD WINAPI ServerMain(LPVOID arg)
+{
+	int retval;
+
+	WSADATA wsa;
+	if (WSAStartup(0x0202, &wsa) != 0)
+		return 1;
+
+	// 소켓 생성
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == listen_sock) err_quit("socket()");
+
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+
+	retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (SOCKET_ERROR == retval) err_quit("bind()");
+
+	retval = listen(listen_sock, SOMAXCONN);
+	if (SOCKET_ERROR == retval) err_quit("listen()");
+
+	SOCKET client_sock;
+	struct sockaddr_in clientaddr;
+	int addrlen;
+	HANDLE hThread;
+
+	while (1)
+	{
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+		if (INVALID_SOCKET == client_sock)
+		{
+			DisplayError("accept()");
+			break;
+		}
+
+		// 접속 클라이언트 정보 출력
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		DisplayText("\r\n[TCP Server] 클라이언트 접속: IP 주소 = %s, 포트 번호 = %d\r\n", addr, ntohs(clientaddr.sin_port));
+
+		hThread = CreateThread(NULL, 0, ProcessClient, (LPVOID)client_sock, 0, NULL);
+		if (NULL == hThread) { closesocket(client_sock); }
+		else { CloseHandle(hThread); }
+	}
+
+	closesocket(listen_sock);
+	WSACleanup();
+	return 0;
+}
+
+// 클라이언트 데이터 통신
+DWORD WINAPI ProcessClient(LPVOID arg)
+{
+	SOCKET client_sock = (SOCKET)arg;
+	int retval;
+	struct sockaddr_in clientaddr;
+	char addr[INET_ADDRSTRLEN];
+	int addrlen;
+	char buf[BUFSIZE + 1];
+
+	// 정보 얻기
+	addrlen = sizeof(clientaddr);
+	getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+
+	while (1)
+	{
+		// 데이터 수신
+		retval = recv(client_sock, buf, BUFSIZE, 0);
+		if (SOCKET_ERROR == retval)
+		{
+			DisplayError("recv()");
+			break;
+		}
+		else if (0 == retval)
+			break;
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		DisplayText("[TCP/%s:%d] %s\r\n", addr, ntohs(clientaddr.sin_port), buf);
+
+		// 데이터 송신
+		retval = send(client_sock, buf, retval, 0);
+		if (SOCKET_ERROR == retval)
+		{
+			DisplayError("send()");
+			break;
+		}
+	}
+
+	closesocket(client_sock);
+	DisplayText("[TCP Server] 클라이언트 종료 : IP 주소 = %s, 포트 번호 = %d\r\n", addr, ntohs(clientaddr.sin_port));
+	return 0;
+}
